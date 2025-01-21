23f4f8526586cef3a9b2cf7e4b042469d7bcd211
Aromal M S <147653903+aromalms@users.noreply.github.com>
2025-01-21 22:48:29 +0530

    Time: 11 ms (52.79%), Space: 153.5 MB (5.58%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.dart
@@ -0,0 +1,30 @@
+class Solution {
+  int romanToInt(String s) {
+     final Map<String, int> romanMap = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  };
+
+  int total = 0;
+  int prevValue = 0;
+
+  for (int i = s.length - 1; i >= 0; i--) {
+    final currentValue = romanMap[s[i]]!;
+
+    if (currentValue < prevValue) {
+      total -= currentValue;
+    } else {
+      total += currentValue;
+    }
+
+    prevValue = currentValue;
+  }
+
+  return total;
+  }
+}
